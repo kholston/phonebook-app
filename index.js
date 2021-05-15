d01b02cb20f1f1887f943e5683c0dfc780b58688
Ken Holston <git.kholston@gmail.com>
2021-05-14 21:18:10 -0400

    exercise 3.3

--- index.js
+++ index.js
@@ -27,13 +27,26 @@ let persons = [
   }
 ]
 
+app.get('/info', (request, response)=>{
+  response.send(`<p>Phonebook has info for ${persons.length} people.</p><p>${new Date()}</p>`)
+})
+
 app.get('/api/persons', (request,response)=>{
   response.json(persons)
 })
 
-app.get('/info', (request, response)=>{
-  response.send(`<p>Phonebook has info for ${persons.length} people.</p><p>${new Date()}</p>`)
+app.get('/api/persons/:id', (request, response)=>{
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if(person){
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+
 })
+
 
 
 const PORT = 3001
