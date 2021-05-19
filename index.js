465713e2c92933df79a98211eee1139363f2309c
Ken Holston <git.kholston@gmail.com>
2021-05-19 12:28:09 -0400

    fix cors error

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { request } = require('express')
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
