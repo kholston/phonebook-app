7cc86e419f64a9419ddec68f19546a745432e99d
Ken Holston <git.kholston@gmail.com>
2021-05-23 14:16:04 -0400

    exercise 3.19

--- index.js
+++ index.js
@@ -51,11 +51,6 @@ app.delete('/api/persons/:id', (request,response, next)=>{
 
 app.post('/api/persons',(request, response, next)=>{
   const body = request.body
-  if(!body.name || !body.number){
-    return response.status(400).json({
-      error: "both name and number required"
-    })
-  }
 
   const person = new Person({
     name: body.name,
@@ -88,6 +83,8 @@ const errorHandler = (error, request, response, next)=>{
   console.log(error.message)
   if(error.name === 'CastError'){
     return response.status(400).send({error: 'malformatted id'})
+  } else if (error.name === 'ValidationError'){
+    return response.status(400).json({error: error.message})
   }
   next(error)
 }
--- models/person.js
+++ models/person.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
@@ -12,9 +13,11 @@ mongoose.connect(url, {useNewUrlParser: true,useUnifiedTopology: true, useFindAn
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: {type: String, required: true ,unique: true},
+  number: {type: String, required: true}
 })
+
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject)=>{
