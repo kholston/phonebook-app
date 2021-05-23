89e62f4bd01a63ddec4df5392cfc1b01ca53b650
Ken Holston <git.kholston@gmail.com>
2021-05-23 12:24:20 -0400

    exercise 3.16

--- index.js
+++ index.js
@@ -26,21 +26,26 @@ app.get('/api/persons', (request,response)=>{
   })
 })
 
-app.get('/api/persons/:id', (request, response)=>{
+app.get('/api/persons/:id', (request, response, next)=>{
   Person.findById(request.params.id).then(person=>{
-    response.json(person)
-  })
+    if(person){
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  }).catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request,response)=>{
+app.delete('/api/persons/:id', (request,response, next)=>{
   Person.findByIdAndDelete(request.params.id)
     .then(result => {
       response.status(204).end()
     })
+    .catch(error => next(error))
 })
 
 
-app.post('/api/persons',(request, response)=>{
+app.post('/api/persons',(request, response, next)=>{
   const body = request.body
   if(!body.name || !body.number){
     return response.status(400).json({
@@ -53,11 +58,22 @@ app.post('/api/persons',(request, response)=>{
     number: body.number,
   })
 
-  person.save().then(savedPerson=>{
-    response.json(savedPerson)
-  })
+  person.save()
+    .then(savedPerson=>{
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
+const errorHandler = (error, request, response, next)=>{
+  console.log(error.message)
+  if(error.name === 'CastError'){
+    return response.status(400).send({error: 'malformatted id'})
+  }
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, ()=>{
